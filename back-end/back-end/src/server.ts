f87e17fab9f9e143e3ef57ae9474adcf00d352aa
Mateus Iasbik <mateusmpi@gmail.com>
2024-12-07 11:22:11 -0300

    test

--- back-end/back-end/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-
-const port = +process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-});
--- back-end/src/config/bingo-rules.ts
+++
@@ -1,6 +0,0 @@
-const BINGORULES = {
-  max: 99,
-  min: 1
-};
-
-export default BINGORULES;
